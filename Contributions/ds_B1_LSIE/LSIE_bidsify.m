95f826374512bb55df8b165ddf23f8c414f246da
Sein <seinjeung@gmail.com>
2025-10-22 18:20:59 +0200

    Parse info in xml
    XML had to be read in as struct

--- Contributions/ds_B1_LSIE/LSIE_bidsify.m
+++ Contributions/ds_B1_LSIE/LSIE_bidsify.m
@@ -1,13 +1,8 @@
-% configure paths 
+% configure paths and tools
 dataFolder      = 'P:\Sein_Jeung\Project_ManySteps\Datasets\LSIE';
 fieldTripPath   = 'P:\Sein_Jeung\Project_BIDS\Workshops\Workshop_2024_NEC\public\Tools\fieldtrip-20240515'; 
 bidsFolder      = fullfile(dataFolder, 'BIDS-data');
-
-% add fieldtrip 
-addpath(fieldTripPath)
-ft_defaults
-[filepath,~,~] = fileparts(which('ft_defaults'));
-addpath(fullfile(filepath, 'external', 'xdf'))
+addpath(fieldTripPath); ft_defaults % add eeglab too if not already on path
 
 % 1. load and inspect EEG files
 % consists of 264 channels EEG data, sampling rate 512 Hz
@@ -26,7 +21,11 @@ imu.IMU.samplingRate    % display sampling rate
 imu.IMU.dateTime        % presumably the onset of the recording 
 
 % 3. load and insepct other relevant metadata files  
+% best read as a struct, contains information similar to dataset
+% description json in BIDS 
 %--------------------------------------------------------------------------
+xml = readstruct(fullfile(dataFolder, 'UM_LSIE.xml')); 
+
 
 % 4. convert data to fiedtrip structs 
 %--------------------------------------------------------------------------
@@ -39,23 +38,22 @@ cfg                                         = [];
 cfg.bidsroot                                = bidsFolder;
 cfg.sub                                     = '004';
 cfg.task                                    = 'LSIE';
-cfg.dataset_description.Name                = 'Data provided by DF group';
-cfg.InstitutionName                         = 'to be filled';
-cfg.InstitutionalDepartmentName             = 'to be filled';
-cfg.InstitutionAddress                      = 'to be filled';
-cfg.TaskDescription                         = 'to be filled';
- 
-% required for dataset_description.json
-cfg.dataset_description.Name                = 'LSIE';
-cfg.dataset_description.BIDSVersion         = 'unofficial extension';
+cfg.dataset_description.Name                = xml.title;
+cfg.dataset_description.BIDSVersion         = '1.10.1';
+cfg.InstitutionName                         = 'University of Michigan';
+cfg.InstitutionalDepartmentName             = xml.organization.name;
+cfg.InstitutionAddress                      = 'n/a';
+cfg.TaskDescription                         = xml.description;
 
 % optional for dataset_description.json
-cfg.dataset_description.License             = 'CC0';
-cfg.dataset_description.Authors             = {"Ferris"};
+cfg.dataset_description.License             = 'CC BY 4.0';
+expArray                                    = xml.experimenters(1).experimenter;
+names                                       = arrayfun(@(x) x.name, expArray, 'UniformOutput', false);
+cfg.dataset_description.Authors             = names;
 cfg.dataset_description.Acknowledgements    = 'n/a';
-cfg.dataset_description.Funding             = {""};
-cfg.dataset_description.ReferencesAndLinks  = {"paper"};
-cfg.dataset_description.DatasetDOI          = 'doi';
+cfg.dataset_description.Funding             = [xml.project.funding.organization, xml.project.funding.grantId];
+cfg.dataset_description.ReferencesAndLinks  = 'n/a';
+cfg.dataset_description.DatasetDOI          = 'n/a';
 
 % 5. enter eeg metadata and feed to data2bids function
 %--------------------------------------------------------------------------
